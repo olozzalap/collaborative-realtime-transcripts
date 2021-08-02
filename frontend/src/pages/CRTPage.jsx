16b84f2516050eede96b7679658f44f7c1f2dc36
Eben Palazzolo <olozzalap@gmail.com>
2021-08-01 21:29:30 -0400

    Init /frontend React App

---
+++ frontend/src/pages/CRTPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CRTPage = () => {
+    return (
+        <div className="CRTPage">
+            <h1>Bloop</h1>
+        </div>
+    );
+}
+
+export default CRTPage;
